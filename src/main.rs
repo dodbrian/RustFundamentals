aadcdee4db45914b32a250d92a3d0255468cd186
Denis Zimin <roydronov@gmail.com>
2019-04-04 00:37:37 +0200

    operators

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,44 @@
 use std::mem;
 
 fn main() {
+    fundamental_data_types();
+    operators();
+}
+
+fn operators() {
+    // arithmetic
+    let mut a = 2 + 3 * 4;
+    println!("{}", a);
+
+    a = a + 1;
+    a -= 2;
+    println!("remainder of {} / {} = {}", a, 3, a % 3);
+
+    let a_cubed = i32::pow(a, 3);
+    println!("{} cubed is {}", a, a_cubed);
+
+    let b = 2.5;
+    let b_cubed = f64::powi(b, 3);
+    let b_to_pi = f64::powf(b, std::f64::consts::PI);
+    println!("{} cubed = {}, {}^pi = {}", b, b_cubed, b, b_to_pi);
+
+    // bitwise
+    let c = 1 | 2; // | OR & AND ^ XOR ! NOR
+    println!("1 | 2 = {}", c);
+
+    let two_to_ten = 1 << 10;
+    println!("2^10 = {}", two_to_ten);
+
+    // logical
+    let pi_less_4 = std::f64::consts::PI < 4.0; // true
+    println!("It's {} that pi is less than 4.0", pi_less_4);
+
+    let x = 5;
+    let x_is_5 = x == 5;
+    println!("It's {} that x is equal to 5", x_is_5);
+}
+
+fn fundamental_data_types() {
     // unsigned 0..255
     let a: u8 = 123; // 8 bits
     println!("a = {}", a);
