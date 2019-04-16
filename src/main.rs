d3e472a7d0db36ac419da7867373c5d3cce350b0
Denis Zimin <denis.zimin@complevo.de>
2019-04-16 18:45:09 +0200

    if statements

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,38 @@ fn main() {
     }
 
     sh::stack_and_heap();
+
+    if_statement();
+}
+
+fn if_statement() {
+    let temp = 15;
+
+    if temp > 30 {
+        println!("really hot outside");
+    } else if temp < 10 {
+        println!("quiet cold");
+    } else {
+        println!("temperature is OK");
+    }
+
+    let day = if temp > 20 { "sunny" } else { "cloudy" };
+    println!("today is {}", day);
+
+    println!(
+        "it is {}",
+        if temp > 20 {
+            if temp > 30 {
+                "very hot"
+            } else {
+                "hot"
+            }
+        } else if temp < 10 {
+            "cold"
+        } else {
+            "OK"
+        }
+    );
 }
 
 fn scope_and_shadowing() {
