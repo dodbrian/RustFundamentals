cec78d0d6810f40e0f3e76128dda7119f0b055cb
Denis Zimin <denis.zimin@complevo.de>
2019-05-07 19:19:06 +0200

    structs and enumerations

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
 mod sh;
 
 use std::mem;
@@ -5,6 +7,29 @@ use std::mem;
 const MEANING_OF_LIFE: u8 = 42; // has no fixed address
 
 static mut GLOBAL_STATIC: i32 = 123;
+
+struct Point {
+    x: f64,
+    y: f64,
+}
+
+struct Line {
+    start: Point,
+    end: Point,
+}
+
+enum Color {
+    Red,
+    Green,
+    Blue,
+    RgbColor(u8, u8, u8), // tuple
+    CmykColor {
+        cyan: u8,
+        magenta: u8,
+        yellow: u8,
+        black: u8,
+    }, // struct
+}
 
 fn main() {
     fundamental_data_types();
@@ -21,6 +46,50 @@ fn main() {
     sh::stack_and_heap();
 
     if_statement();
+
+    while_and_loop();
+    for_loop();
+    match_statement();
+
+    structures();
+    enums();
+}
+
+fn enums() {
+    // let c: Color = Color::RgbColor(10, 0, 0);
+    let c: Color = Color::CmykColor {
+        cyan: 0,
+        magenta: 128,
+        yellow: 0,
+        black: 255,
+    };
+
+    match c {
+        Color::Red => println!("r"),
+        Color::Green => println!("g"),
+        Color::Blue => println!("b"),
+        Color::RgbColor(0, 0, 0)
+        | Color::CmykColor {
+            cyan: _,
+            magenta: _,
+            yellow: _,
+            black: 255,
+        } => println!("black"),
+        Color::RgbColor(r, g, b) => println!("rgb({},{},{})", r, g, b),
+        _ => (),
+    }
+}
+
+fn structures() {
+    let p = Point { x: 3.0, y: 4.0 };
+    println!("point p is at ({}, {})", p.x, p.y);
+
+    let p2 = Point { x: 5.0, y: 10.0 };
+    let myline = Line { start: p, end: p2 };
+    println!(
+        "Line starts at ({}, {}) and ends at ({}, {})",
+        myline.start.x, myline.start.y, myline.end.x, myline.end.y
+    );
 }
 
 fn if_statement() {
@@ -51,10 +120,6 @@ fn if_statement() {
             "OK"
         }
     );
-
-    while_and_loop();
-    for_loop();
-    match_statement();
 }
 
 fn match_statement() {
